4eeda56710ad0ae4ea5d6f47a5a625a3f9eed610
lingkongyingsun <zhengmiao_cau@163.com>
2020-03-19 15:10:06 +0800

    fish

--- split_data.py
+++ split_data.py
@@ -23,7 +23,7 @@ for path in glob.glob(os.path.join(fish_path.replace('fish_process','csv_modifie
 
 #划分数据集
 fish_train,fish_test1,label_train,label_test1= train_test_split(data_list,label_list,test_size=0.4,random_state=0)
-fish_val,fish_test,label_val,label_test= train_test_split(fish_test1,label_test1,test_size=0.2,random_state=0)
+fish_val,fish_test,label_val,label_test= train_test_split(fish_test1,label_test1,test_size=0.5,random_state=0)
 with open('fish_train.txt','w') as outfile:
     outfile.write(','.join(fish_train))
 with open('fish_val.txt','w') as outfile:
